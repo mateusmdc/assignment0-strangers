15f0a12ae4bbdb810f2a2f537b2276479ec725c5
WilliamVieira96 <38863678+WilliamVieira96@users.noreply.github.com>
2018-04-30 23:54:52 -0300

    Add files via upload

--- questao1.m
+++ questao1.m
@@ -1,45 +1,12 @@
-%Questão 1) Faça um script para Matlab ou Octave que leia dois arquivos,
-%cada um contendo um vetor tridimensional (X e Y). Faça as operações a seguir:
+%Questão 2) Faça um script para Matlab ou Octave que leia dois arquivos,
+%cada um contendo uma matriz (A e B). Se elas forem compatíveis faça a 
+%multiplicação entre elas (usando a estrutura de laço) e escreva em um arquivo
+%a matriz resultante transposta. 
+%Compare o resultado com a multiplicação que o próprio programa oferece.
+%Caso as matrizes não sejam compatíveis, avise.
 
-%a) soma, b) subtração
+load matrizA.dat
+load matrizB.dat
 
-load vetorX.dat
-load vetorY.dat
-
-function Soma = somavet(a,b)
-  
-  Soma = a+b
-  
-endfunction
-
-function Subtracao = subvet(a,b)
-  
-  Subtracao = a-b
-  
-endfunction
-
-function Produto = prodporesc(a,b)
-  
-  Produto = a*b;
-  
-endfunction
-
-for i = 1:3
- 
-  vetorsoma(i) = somavet(vetorX(i),vetorY(i));
-  vetorsubtracao(i) = subvet(vetorX(i),vetorY(i));
- 
-end
-
-vetorsoma
-vetorsubtracao
-
-%c) multiplicação por escalar de X (primeiro elemento do vetor) 
-
-escalar = input("Digite o escalar: ");
-
-vetorproduto = vetorX;
-
-vetorproduto(1) = prodporesc(escalar,vetorX(1));
-
-vetorproduto
+C = matrizA * matrizB
+D = C'
